2e7f61a1dcc1726bd36bb7823d00b87c19c7008b
szhuo <1366808715@qq.com>
2023-12-22 17:43:15 +0800

    add initial file

---
+++ config.py
@@ -0,0 +1,41 @@
+import torch
+
+class Config:
+    def __init__(self) -> None:
+        self.proj_name = 'diff'
+        
+        # ddpm
+        self.timesteps = 1000
+        self.image_size = 64
+        self.image_channels = 3
+        self.beta0 = 1e-4 # start
+        self.beta1 = 0.02 # end
+
+        '''
+        train hyperparameters
+        '''
+        self.batch = 64
+        self.lr = 1e-4
+        self.epochs = 10
+        self.save_period = 5
+        self.sample_period =1
+
+        '''
+        inference
+        '''
+        self.steps = 500
+        self.num_images = 32
+
+        '''
+        model hyperparameters
+        '''
+        self.base_channels = 64
+        self.timestep_embed_dim = 64
+        self.timestep_proj_dim = 256
+        self.layers = 2
+
+        '''
+        others
+        '''
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.clip = 1.0
